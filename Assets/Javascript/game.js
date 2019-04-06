51046abb46b39648ac8e5f0b6cfe1ad651f9db9a
Beatriz <renderosb@gmail.com>
2019-04-06 19:15:36 -0400

    game

---
+++ Assets/Javascript/game.js
@@ -0,0 +1,11 @@
+var computerChoice = [18-120];
+        var wins = 0;
+        var losses = 0;
+        var randomNumber= document.getElementById("randomNumber-text");
+        var winsText = document.getElementById("wins-text");
+        var lossesText = document.getElementById("losses-text");
+
+    
+    document.click = function(event) {
+       var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+
