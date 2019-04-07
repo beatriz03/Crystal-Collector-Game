2bcc368c33ed14fba9bce64559b8f90ffd6ed260
Beatriz <renderosb@gmail.com>
2019-04-06 23:23:09 -0400

    add

--- Assets/Javascript/game.js
+++ Assets/Javascript/game.js
@@ -1,11 +1,21 @@
 var computerChoice = [18-120];
-        var wins = 0;
-        var losses = 0;
-        var randomNumber= document.getElementById("randomNumber-text");
+var crystalValues = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+var randomNumber = 0;    
+var wins = 0;
+var losses = 0;
+
+    var randomNumber= document.getElementById("randomNumber-text");
         var winsText = document.getElementById("wins-text");
         var lossesText = document.getElementById("losses-text");
 
     
-    document.click = function(event) {
-       var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+    $(".bluecrystal").on("click", function() {
+        var randomNumber = randomNumber[Math.floor(Math.random() * randomNumber.length)];
+
+
+       if (pointSoFar > randomNumber) {
+        losses++
+       } else if (pointsSoFar == randomNumber) {
+        wins++
+       }
 
--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # unit-4-game
+
+The Crystal Colector game is an exercise with Jquery. We have learned how to create a page with HTML and style it with CSS but in order to make it dynamic we use Jquery and Javascript. 
+
+This project is useful because it refreshes the programmer in HTML/CSS but implements the new material from unit 4 to make it interactive, i.e clicking a button to add to your score. 
+
+Users can get started on this project by psuedo-coding what the user is supposed to do to win. example: 
+        When user clicks on crystal a random number is generated and added to their score.
+        When different crystals are clicked their values are summed together.
+        When the user's "points so far" exceeds the "number to match" the game is over, losses increase.
+        etc.
+
+
